6c1bf3987f358811c2c1f3c5ee5b74f752b2ec54
v-zhaihongyuan <v-zhaihongyuan@xiaomi.com>
2020-07-22 17:21:33 +0800

    熟悉基本语法
    Signed-off-by: v-zhaihongyuan <v-zhaihongyuan@xiaomi.com>

---
+++ app/src/main/java/com/example/kotlin/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.kotlin
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+}
--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+    private val mFragments = mutableListOf<Fragment>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFragment()
+    }
+
+    private fun initFragment() {
+        mFragments.add(HomeFragment())
+        println("------" + mFragments.size)
     }
 }
